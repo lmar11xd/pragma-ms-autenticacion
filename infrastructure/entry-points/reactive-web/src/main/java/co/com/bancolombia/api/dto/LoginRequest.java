4a9ab79bf07a0e936a0464f03bd0c04b7b8985e5
Luis Alvarado <lmar11xd@gmail.com>
2025-09-06 12:16:08 -0500

    Documentacion de login request

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/LoginRequest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/dto/LoginRequest.java
@@ -1,10 +1,13 @@
 package co.com.bancolombia.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
+        @Schema(description = "Correo del solicitante", example = "pedroj@example.com")
         @Email @NotBlank String email,
+        @Schema(description = "Contraseña del solicitante", example = "123456")
         @NotBlank String password
 ) {
 }
